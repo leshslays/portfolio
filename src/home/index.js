c375b7c1160cbe42ba7a7d9e2ad0544ed86ad199
leshslays <44446951+leshslays@users.noreply.github.com>
2020-06-19 23:23:13 -0400

    Added P1

--- src/home/index.js
+++ src/home/index.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import  "../App.css"
 
 export default class Home extends Component{
     render() {
         return(
             <>
-                <h1>Home</h1>
-                <a href = "/about">About</a>
+                <h1><center>Projects</center></h1>
+                <div className="img-with-text">
+                    <ul>
+                        <Link to="/about">
+
+                                <img src="https://gbksoft.com/blog/wp-content/uploads/2019/07/trading-984x555.png" alt="Algorithmic Trading Pic"/>
+                                <p><a>Algorithmic Trading</a></p>
+                        </Link>
+                    </ul>
+                </div>
+
             </>
         )
     }
--- src/main/index.js
+++ src/main/index.js
@@ -1,23 +1,192 @@
-import React, { Component } from 'react';
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import InfoIcon from '@material-ui/icons/Info';
 import {Switch, Route, BrowserRouter as Router} from 'react-router-dom';
+import Button from '@material-ui/core/Button';
 import Home from '../home/index';
 import About from '../about/index';
-import Nav from '../components/Toolbar/Toolbar';
+import
 
+const drawerWidth = 240;
 
-export default class Main extends Component{
-    render(){
-        return(
-            <>
-                <Nav/>
-            <Router>
-                <Switch>
-                    <Route exact path="/" component={Home}/>
-                    <Route exact path="/about" component={About}/>
-                </Switch>
-            </Router>
-                </>
-        )
-    }
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
+    appBar: {
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    hide: {
+        display: 'none',
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-end',
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginLeft: -drawerWidth,
+    },
+    contentShift: {
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+    },
+}));
 
+export default function PersistentDrawerLeft() {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <AppBar
+                position="fixed"
+                className={clsx(classes.appBar, {
+                    [classes.appBarShift]: open,
+                })}
+            >
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        className={clsx(classes.menuButton, open && classes.hide)}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" noWrap>
+                        Leshlye Rivera
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                className={classes.drawer}
+                variant="persistent"
+                anchor="left"
+                open={open}
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+            >
+                <div className={classes.drawerHeader}>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                    </IconButton>
+                </div>
+                <Divider />
+                <List>
+                    <ListItem>
+                        <Button href='/about' style={{ width: '100%', textAlign: 'left' }}><InfoIcon /> &nbsp; &nbsp; Home</Button>
+                    </ListItem>
+                    {/*{['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (*/}
+                    {/*    <ListItem button key={text}>*/}
+                    {/*        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>*/}
+                    {/*        <ListItemText primary={text} />*/}
+                    {/*    </ListItem>*/}
+                    {/*))}*/}
+                </List>
+                <Divider />
+                <List>
+                    {['Projects', 'About', 'Contact'].map((text, index) => (
+                        <ListItem button key={text}>
+                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
+            <main
+                className={clsx(classes.content, {
+                    [classes.contentShift]: open,
+                })}
+            >
+                <div className={classes.drawerHeader} />
+                <Router>
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route exact path="/about" component={About}/>
+                    </Switch>
+                </Router>
+            </main>
+        </div>
+    );
 }
+
+// import React, { Component } from 'react';
+// import {Switch, Route, BrowserRouter as Router} from 'react-router-dom';
+
+// import Nav from '../components/Toolbar/Toolbar';
+//
+//
+// export default class Main extends Component{
+//     render(){
+//         return(
+//             <>
+//                 <Nav/>
+
+//             </>
+//         )
+//     }
+//
+// }
